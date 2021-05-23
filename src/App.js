04102454cc5155820421d276d2984b3617e4556e
deyaa1997 <deyah.mosa@gmail.com>
2021-05-23 18:57:34 +0300

    Pulse Check 6

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import List from './components/List'
 import ListItem from './components/ListItem'
 
+
+const todos = [{todo: "wake up", id: 1}, {todo: "breakfast", id: 2}, {todo: "study", id: 3}]
 // jsx
 const App = () => {
   return <h1>hello world</h1>;
