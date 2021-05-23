4a5fbedb30a3545be2c8a43118bbfa8f193c8b2a
deyaa1997 <deyah.mosa@gmail.com>
2021-05-23 20:24:22 +0300

    Finish all Check Pulse

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -1,10 +1,15 @@
 import React from "react" ; 
 
 
+
 const ListItem = ({todoList , id})=>{
+    const logId = () => {
+        console.log(id);
+      };
     return(
         <>
         <li>{todoList}</li>
+        <button onClick={logId}>click</button>
         </>
     )
 }
